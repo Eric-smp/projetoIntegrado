42c7e3a28a974d00ceb9dd82d5160d323baf01da
Eric Sampaio <eric.sampaio@devbot.com.br>
2023-05-24 19:10:04 -0300

    Alteração no html, css e javaScript

---
+++ projeto_integrador/app_aviacao/templates/script.js
@@ -0,0 +1,28 @@
+let checkOne = document.getElementById('idaVolta');
+let checkTwo = document.getElementById("somenteIda")
+let dateIda = document.getElementById('dataIda')
+let dateVolta = document.getElementById('dataVolta')
+let quatAdultos = document.getElementById('adultos')
+let quatCrianca = document.getElementById('crianças')
+let quatBebe = document.getElementById('bebes')
+
+
+
+
+
+
+function result(){
+  const partida = document.getElementById('partida').value
+  const destino = document.getElementById('destino').value
+
+  if(partida != destino){
+    console.log(true)
+  }else{
+    console.log(false)
+  }
+}
+
+
+function button(){
+
+}
--- projeto_integrador/projeto_integrador/settings.py
+++ projeto_integrador/projeto_integrador/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'america/Sao-Paulo'
 
 USE_I18N = True
 
@@ -118,11 +118,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = '/static/'
+
 
